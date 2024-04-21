f46bdaf67e1d1521dd8ae59c30f2056fba72ce6e
herouu <herouucn@foxmail.com>
2024-04-21 12:10:11 +0800

    feat: scoop安装脚本

---
+++ shell/scoop/install.ps1
@@ -0,0 +1,51 @@
+# 开启调试
+# Set-PSDebug -Trace 1
+
+# 需先以管理员权限打开系统自带的 Powershell，设置脚本运行权限
+# Set-Executionpolicy -ExecutionPolicy RemoteSigned -Scope Currentuser
+
+# 安装 Scoop
+(Invoke-RestMethod https://mirror.ghproxy.com/https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1).Replace('"https://github.com', '"https://mirror.ghproxy.com/https://github.com') | Invoke-Expression
+
+# 将 Scoop 的仓库源替换为代理的
+scoop config scoop_repo https://mirror.ghproxy.com/https://github.com/ScoopInstaller/Scoop
+
+# 目前没有安装 Git，所以先下载几个必需的软件的 JSON，组成一个临时的应用仓库
+New-Item -ItemType "directory" -Path "$env:SCOOP\buckets\scoop-cn\bucket"
+New-Item -ItemType "directory" -Path "$env:SCOOP\buckets\scoop-cn\scripts\7-zip"
+New-Item -ItemType "directory" -Path "$env:SCOOP\buckets\scoop-cn\scripts\git"
+
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/bucket/7zip.json -OutFile "$env:SCOOP\buckets\scoop-cn\bucket\7zip.json"
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/scripts/7-zip/install-context.reg -OutFile "$env:SCOOP\buckets\scoop-cn\scripts\7-zip\install-context.reg"
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/scripts/7-zip/uninstall-context.reg -OutFile "$env:SCOOP\buckets\scoop-cn\scripts\7-zip\uninstall-context.reg"
+
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/bucket/git.json -OutFile "$env:SCOOP\buckets\scoop-cn\bucket\git.json"
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/scripts/git/install-context.reg -OutFile "$env:SCOOP\buckets\scoop-cn\scripts\git\install-context.reg"
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/scripts/git/uninstall-context.reg -OutFile "$env:SCOOP\buckets\scoop-cn\scripts\git\uninstall-context.reg"
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/scripts/git/install-file-associations.reg -OutFile "$env:SCOOP\buckets\scoop-cn\scripts\git\install-file-associations.reg"
+Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/scripts/git/uninstall-file-associations.reg -OutFile "$env:SCOOP\buckets\scoop-cn\scripts\git\uninstall-file-associations.reg"
+
+# Invoke-RestMethod -Uri https://mirror.ghproxy.com/https://raw.githubusercontent.com/duzyn/scoop-cn/master/bucket/aria2.json -OutFile "$env:SCOOP\buckets\scoop-cn\bucket\aria2.json"
+
+# 安装时注意顺序是 7-Zip, Git, Aria2
+scoop install scoop-cn/7zip
+scoop install scoop-cn/git
+# scoop install scoop-cn/aria2
+
+# scoop-cn 库还不是 Git 仓库，删掉后，重新添加 Git 仓库
+if (Test-Path -Path "$env:SCOOP\buckets\scoop-cn") {
+    scoop bucket rm scoop-cn
+}
+Write-Host "Adding scoop-cn bucket..."
+scoop bucket add scoop-cn https://mirror.ghproxy.com/https://github.com/duzyn/scoop-cn
+scoop bucket add spc https://mirror.ghproxy.com/https://github.com/lzwme/scoop-proxy-cn
+
+# 删除 Scoop 的 main 仓库
+if (Test-Path -Path "$env:SCOOP\buckets\main") {
+    scoop bucket rm main
+}
+
+# Set-Location "$env:SCOOP\buckets\scoop-cn"
+# git config pull.rebase true
+
+Write-Host "scoop and scoop-cn was installed successfully!"
